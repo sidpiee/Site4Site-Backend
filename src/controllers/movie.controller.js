1ab427357689ab48f02132a1753c2a1ddc01ba4c
Siddharth Sharma <sids23104@gmail.com>
2026-05-07 12:45:09 +0530

    feat: add particular movie route

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -19,6 +19,46 @@ const findmovie = asyncHandler(async (req, res) => {
       .json(new APIResponse(200, cachedData, "Fetched from cache"));
   }
   const result = await fetch(
+    `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=${encodeURIComponent(movie)}`,
+    {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        Accept: "application/json",
+      },
+    },
+  );
+  if (!result.ok) {
+    const text = await result.text();
+    console.log("Movie API error:", result.status, text);
+    throw new APIError(
+      result.status,
+      "movie service temporarily unavailable. Please try again.",
+    );
+  }
+  const data = await result.json();
+  if (data.Response === "False")
+    throw new APIError(404, data.Error || "No movie found");
+  myCache.set(normalizedMovie, data);
+  res
+    .status(200)
+    .json(new APIResponse(200, data, "movie fetched successfully"));
+});
+
+const findParticularMovie = asyncHandler(async (req, res) => {
+  const { movie } = req.query;
+  if (!movie?.trim()) {
+    throw new APIError(400, "Movie name is required!");
+  }
+
+  const normalizedMovie = movie.trim().toLowerCase();
+
+  const cachedData = myCache.get(normalizedMovie);
+  if (cachedData) {
+    return res
+      .status(200)
+      .json(new APIResponse(200, cachedData, "Fetched from cache"));
+  }
+  const result = await fetch(
     `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&t=${encodeURIComponent(movie)}`,
     {
       headers: {
@@ -36,11 +76,12 @@ const findmovie = asyncHandler(async (req, res) => {
     );
   }
   const data = await result.json();
-  if (data.Response === "False") throw new APIError(404, "No movie found");
+  if (data.Response === "False")
+    throw new APIError(404, data.Error || "No movie found");
   myCache.set(normalizedMovie, data);
   res
     .status(200)
     .json(new APIResponse(200, data, "movie fetched successfully"));
 });
 
-export { findmovie };
+export { findmovie, findParticularMovie };
--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { findmovie } from "../controllers/movie.controller.js";
+import {
+  findmovie,
+  findParticularMovie,
+} from "../controllers/movie.controller.js";
 
 const router = Router();
 
 router.route("/").get(findmovie);
+router.route("/this").get(findParticularMovie);
 
 export default router;
