5f00b5d0c01cdd9785096b69a822f073097622d9
sidpiee <sids23104@gmail.com>
2026-07-06 23:20:52 +0530

    feat: update section and site route

--- src/controllers/section.controller.js
+++ src/controllers/section.controller.js
@@ -64,5 +64,36 @@ const deleteSite = asyncHandler(async(req,res)=> {
 }
   res.status(200).json(new APIResponse(200 , s , "Site deleted successfully"));
 })
+const updateSection = asyncHandler(async(req , res) => {
+  const {id} = req.params;
+  const {title , description} = req.body;
+  if(!title) throw new APIError(400 , "Title cannot be empty");
+  const s = await Section.findOneAndUpdate({
+    _id : id ,
+    userId : req.user.id,
+  }, {
+    title , description
+  } , {new : true ,  runValidators: true,});
+  res.status(200).json(new APIResponse(200 , s , "Section updated succcessfully"));
+})
+const updateSite = asyncHandler(async(req,res) => {
+  const {sectionId , id} = req.params;
+  const section = await Section.findOne({
+  _id: sectionId,
+  userId: req.user.id,
+});
 
-export {addSection ,deleteSection , getSection , addSite , deleteSite};
+if (!section)
+  throw new APIError(404, "Section not found");
+  const {name , url , note} = req.body;
+  if(!name || !url) throw new APIError(400 , "Name or Url cannot be empty");
+  const s = await Site.findOneAndUpdate({
+    _id : id ,
+    sectionId
+  } , {
+    name , url  , note
+  } ,{ new : true ,  runValidators: true,})
+  res.status(200).json(new APIResponse(200 , s , "Site updated succcessfully"));
+})
+
+export {addSection ,deleteSection , getSection , addSite , deleteSite , updateSection , updateSite};
--- src/routes/section.routes.js
+++ src/routes/section.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express"
 import { requireAuth } from "../middlewares/auth.middleware.js";
-import { addSection, addSite, deleteSection, deleteSite, getSection } from "../controllers/section.controller.js";
+import { addSection, addSite, deleteSection, deleteSite, getSection, updateSection, updateSite } from "../controllers/section.controller.js";
 const router = Router();
 
 router.route("/section").post(requireAuth,addSection).get(requireAuth , getSection );
-router.route("/section/:id").delete(requireAuth,deleteSection);
+router.route("/section/:id").delete(requireAuth,deleteSection).patch(requireAuth , updateSection);
 router.route("/section/site/:id").post(requireAuth , addSite );
-router.route("/section/:sectionId/site/:id").delete(requireAuth , deleteSite);
+router.route("/section/:sectionId/site/:id").delete(requireAuth , deleteSite).patch(requireAuth , updateSite);
 export default router;
